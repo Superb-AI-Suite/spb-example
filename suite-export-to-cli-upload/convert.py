d549ddad9eeb5b917e04c6d28086257568754ab1
jonghbae <jhbae@superb-ai.com>
2022-07-20 06:24:33 +0000

    testing finished for suite to cli

--- suite-export-to-cli-upload/convert.py
+++ suite-export-to-cli-upload/convert.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 import simplejson as json
+import os
 
 
 def process(args):
-    export_dir, output_path = args.export_dir
+    export_dir, output_path = args.export_dir, args.output_path
     meta_map = {}
     for p in Path(export_dir, 'meta').rglob('*.json'):
         with open(p, 'r', encoding='utf-8') as f:
@@ -14,7 +15,16 @@ def process(args):
         with open(Path(export_dir) / f'{meta_map[data_key]}', 'r', encoding='utf-8') as f:
             label = json.load(f)
             result['result'] = label
-            with open(Path(output_path) / f'{data_key}' + '.json', 'r', encoding='utf-8') as c:
+            print(result)
+            folder = data_key.split('/')[:-1]
+            output_folder = ''
+            for obj in folder:
+                output_folder = output_folder + obj + '/'
+            print(output_folder)
+            os.makedirs(os.path.join(
+                output_path, output_folder), exist_ok=True)
+
+            with open(Path(output_path) / f'{data_key}.json', 'w+', encoding='utf-8') as c:
                 json.dump(result, c)
 
 
